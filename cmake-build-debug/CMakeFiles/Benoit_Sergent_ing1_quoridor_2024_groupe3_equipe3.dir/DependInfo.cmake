5ec28ba57af4ad004d9ba25b0bef1445beefab2b
Théo Boursier <theo.boursier@edu.ece.fr>
2024-10-30 21:42:22 +0100

    modif "beauté" du code

---
+++ cmake-build-debug/CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/theob/OneDrive/Documents/GitHub/Benoit-Sergent-ing1-quoridor-2024-groupe3-equipe3/Jeu.c" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/Jeu.c.obj" "gcc" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/Jeu.c.obj.d"
+  "C:/Users/theob/OneDrive/Documents/GitHub/Benoit-Sergent-ing1-quoridor-2024-groupe3-equipe3/Menu.c" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/Menu.c.obj" "gcc" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/Menu.c.obj.d"
+  "C:/Users/theob/OneDrive/Documents/GitHub/Benoit-Sergent-ing1-quoridor-2024-groupe3-equipe3/Plateau.c" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/Plateau.c.obj" "gcc" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/Plateau.c.obj.d"
+  "C:/Users/theob/OneDrive/Documents/GitHub/Benoit-Sergent-ing1-quoridor-2024-groupe3-equipe3/main.c" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/main.c.obj" "gcc" "CMakeFiles/Benoit_Sergent_ing1_quoridor_2024_groupe3_equipe3.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
