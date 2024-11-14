e6da619fde390f1cbcb3de4bc075a0661398e742
Théo Boursier <theo.boursier@edu.ece.fr>
2024-11-14 12:40:17 +0100

    Update1 calcul des scores

--- ScoreCalcul.c
+++ ScoreCalcul.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include "ScoreCalcul.h"
 
-
-void attribuerScore() {
-
+void attribuerScore() {      // cherhcer fichier text si pseudo deja existant sinon ajouter pseudo
+    FILE* pf = fopen("./score.txt", "w");
+    if (pf == NULL) {
+        printf("Erreur d'ouverture de fichier.");
+    }
 }
 
-void ajouterScore() {
-
+void ajouterScore() {       // changer score du gagnant en ajoutant + 5
+    FILE* pf = NULL;
 }
