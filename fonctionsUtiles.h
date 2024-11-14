b568f5dbe8a216f11e13339493e3614e744a88e8
Théo Boursier <theo.boursier@edu.ece.fr>
2024-11-14 11:10:48 +0100

    réécriture

--- fonctionsUtiles.h
+++ fonctionsUtiles.h
@@ -5,8 +5,8 @@
 //Fonction qui permet de mettre de la couleur dans la console
 void Color(int couleurDuTexte,int couleurDeFond) // fonction d'affichage de couleurs
 {
-    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(H,couleurDeFond*16+couleurDuTexte);
+    HANDLE H = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H,couleurDeFond * 16 + couleurDuTexte);
 }
 
 #endif //BENOIT_SERGENT_ING1_QUORIDOR_2024_GROUPE3_EQUIPE3_FONCTIONSUTILES_H
