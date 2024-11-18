4b75201fdba651f85ff8b8b4e706a7d3b2b255c6
Clement-L24 <clement.lafolie@edu.ece.fr>
2024-11-18 10:28:44 +0100

    Fin des conditions barrieres (j'espère)

--- Souris.h
+++ Souris.h
@@ -3,7 +3,7 @@
 #include "Jeu.h"
 
 int souris_joueurs(int positionValide[6][2], int* X, int* Y);
-int souris_barrieres(int* BX1, int* BY1, int* BX2, int* BY2);
+int souris_barrieres(int* BX1, int* BY1, int* BX2, int* BY2, int plateau[17][17]);
 void definitioncoordonneesXYbarrieres(int* Xsouris, int* Ysouris, int* X, int* Y);
 
 #endif //SOURIS_H
