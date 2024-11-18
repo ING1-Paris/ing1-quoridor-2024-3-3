c4f717a3ab155d6301a4d8451c86f410f50bc053
Clement-L24 <clement.lafolie@edu.ece.fr>
2024-11-18 15:04:19 +0100

    Update Modularité

--- Souris.h
+++ Souris.h
@@ -4,7 +4,7 @@
 
 int souris_joueurs(int positionValide[6][2], int* X, int* Y);
 int souris_barrieres(int* BX1, int* BY1, int* BX2, int* BY2, int plateau[17][17]);
-void definitioncoordonneesXYbarrieres(int* Xsouris, int* Ysouris, int* X, int* Y);
+void definitioncoordonneesXY(int* Xsouris, int* Ysouris, int* X, int* Y);
 int remplacer_barrieres(int* BX1, int* BY1, int* BX2, int* BY2, int plateau[17][17]);
 
 #endif //SOURIS_H
